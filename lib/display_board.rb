06217baa2fc50efcbd957fd5b4ae5610b5ed4f83
maddymthompson <madeline.thompson.17@tfacorps.org>
2018-06-25 21:34:05 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,13 +1,9 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-board = ["X","X","X","X","X","X","X","X","X"]
-
 def display_board(board)
-  puts " #{baord[0]}  | #{baord[1]}| #{baord[2]} "
+  puts " #{baord[0]} | #{baord[1]}| #{baord[2]} "
   puts "---------------"
-  puts " #{baord[3]}  | #{baord[4]}| #{baord[5]} "
+  puts " #{baord[3]} | #{baord[4]}| #{baord[5]} "
   puts "---------------"
-  puts " #{baord[6]}  | #{baord[7]}| #{baord[8]} "
+  puts " #{baord[6]} | #{baord[7]}| #{baord[8]} "
 end
-
-display_board
